9a7705d553b0070c3459cb85e609a6795fb29c24
pppereira3 <=>
2018-04-19 17:42:16 +0100

    AAAAA

---
+++ desktop/src/com/mygdx/weed/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.weed.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.weed.TetrWeed;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TetrWeed(), config);
+	}
+}
