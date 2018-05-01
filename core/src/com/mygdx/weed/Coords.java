527d75dbb311567db0aafb950b7774188686b0ee
pppereira3 <=>
2018-05-01 18:11:38 +0100

    Add Coords, Block and Tetromino class squeleton's

---
+++ core/src/com/mygdx/weed/Coords.java
@@ -0,0 +1,87 @@
+package com.mygdx.weed;
+
+/**
+ * Class for coordinates.
+ */
+public class Coords {
+
+    private int x;
+    private int y;
+
+    /*
+     * Constructs the object
+     *
+     * @param      x     X coordinate
+     * @param      y     Y coordinate
+     */
+    public Coords(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Equals
+     *
+     * @param      t     Object to be compared to the calling coordinate
+     *
+     * @return     true if Object t is a Coordinate and has the same values as the calling obj
+     */
+    public boolean equals(Object t) {
+        return t != null && t instanceof Coords && ((Coords) t).X() == this.x &&
+                ((Coords) t).Y() == this.y;
+    }
+
+    /**
+     * Get X
+     *
+     * @return     x
+     */
+    public int X() {
+        return this.x;
+    }
+
+    /**
+     * Get Y
+     *
+     * @return     y
+     */
+    public int Y() {
+        return this.y;
+    }
+
+    /**
+     * Sets the coordinates.
+     *
+     * @param      x     new x
+     * @param      y     new y
+     */
+    public void setCoords(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    /**
+     * Copy Constructor
+     *
+     * @param      nc    Coords to be copied
+     */
+    public void setCoords(Coords nc) {
+        this.x = nc.X();
+        this.y = nc.Y();
+    }
+
+    public int compareTo(Coords c){
+        /*int dx = abs(this.X() - c.X());
+        int dy = abs(this.Y() - c.Y());
+        if (dx < dy){
+            if (this.Y() < c.Y()) return -1;
+            return 1;
+        }
+        else{
+            if (this.X() < c.X()) return -1;
+            return 1;
+        }*/
+        return 0;
+    }
+}
---
+++ core/src/com/mygdx/weed/Tetromino.java
@@ -0,0 +1,8 @@
+package com.mygdx.weed;
+
+/**
+ * Created by pppereira3 on 29-04-2018.
+ */
+
+public class Tetromino {
+}
