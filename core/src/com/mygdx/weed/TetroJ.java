27d4d2c262c3e3f562e4393fc67af9b0599b4720
pppereira3 <=>
2018-05-12 21:39:42 +0100

    SHAPES

---
+++ core/src/com/mygdx/weed/TetroJ.java
@@ -0,0 +1,8 @@
+package com.mygdx.weed;
+
+/**
+ * Created by pppereira3 on 12-05-2018.
+ */
+
+public class TetroJ extends Tetromino {
+}
---
+++ core/src/com/mygdx/weed/TetroO.java
@@ -0,0 +1,8 @@
+package com.mygdx.weed;
+
+/**
+ * Created by pppereira3 on 03-05-2018.
+ */
+
+public class TetroO extends Tetromino {
+}
