2a79d35a47de9241816497ddc19b4360d8c89dc5
Unknown <up201603811@fe.up.pt>
2018-05-15 14:53:50 +0100

    merge

--- core/src/com/mygdx/weed/TetroS.java
+++ core/src/com/mygdx/weed/TetroS.java
@@ -8,7 +8,6 @@ public class TetroS extends Tetromino {
 
     public TetroS(char power){
         super('S');
-<<<<<<< Updated upstream
         Block a = new Block(new Coords (0, 0), 'G', 'N');
         Block b = new Block(new Coords (1, 0), 'G', 'N');
         Block c = new Block(new Coords (1, 1), 'G', power);
@@ -17,22 +16,13 @@ public class TetroS extends Tetromino {
         this.blocks.put("B", b);
         this.blocks.put("C", c);
         this.blocks.put("D", d);
-=======
-        Block a = new Block(new Coords (0, 1), 'G', 'N');
-        Block b = new Block(new Coords (1, 1), 'G', 'N');
-        Block c = new Block(new Coords (1, 0), 'G', power);
-        Block d = new Block(new Coords (2, 0), 'G', 'N');
-        this.blocks.add(a);
-        this.blocks.add(b);
-        this.blocks.add(c);
-        this.blocks.add(d);
->>>>>>> Stashed changes
+
     }
 
     public void rotate(){
         switch (this.dir) {
             case 0:
-                this.blocks.get(0).setCoords(2, 2);
+                
                 break;
             case 1:
                 break;
