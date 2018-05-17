e8cef26d21f31ce0acd945c5136ed9aec4c68109
pppereira3 <=>
2018-05-17 15:02:48 +0100

    ASF

--- core/src/com/mygdx/weed/Tetris.java
+++ core/src/com/mygdx/weed/Tetris.java
@@ -1,12 +1,12 @@
 package com.mygdx.weed;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Tetris extends ApplicationAdapter {
+public class Tetris extends Game {
 	SpriteBatch batch;
 	Texture img;
 	/*
@@ -14,12 +14,14 @@ public class Tetris extends ApplicationAdapter {
 	* */
 	@Override
 	public void create () {
+		//this.create();
 		batch = new SpriteBatch();
 		img = new Texture("dick.jpg");
 	}
 
 	@Override
 	public void render () {
+		//this.render();
 		Gdx.gl.glClearColor(255, 255, 255, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
