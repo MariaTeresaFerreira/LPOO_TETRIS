2dcb57f06e86fbe40bb103afabc84c2c00dfddf6
Unknown <up201603811@fe.up.pt>
2018-05-19 18:13:45 +0100

    main menu buttons

--- core/src/com/mygdx/mainpackage/Screens/MainMenuScreen.java
+++ core/src/com/mygdx/mainpackage/Screens/MainMenuScreen.java
@@ -1,19 +1,27 @@
 package com.mygdx.mainpackage.Screens;
 
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.mygdx.mainpackage.Tetris;
 
 public class MainMenuScreen implements Screen{
 
-    Tetris t;
-    Texture background;
-    Texture muteButton;
-    Texture startButton;
-    Texture highScoresButton;
-    Texture exitButton;
+    private Tetris t;
+    private Texture background;
+    private Sprite muteSprite;
+    private Sprite startSprite;
+    private Sprite highScoresSprite;
+    private Sprite exitSprite;
+
+    private Button exitButton;
+    private Button startButton;
+    private Button highScoresButton;
+
     public static final float BUTTON_WIDTH = 150;
     public static final float BUTTON_HEIGHT = 75;
 
@@ -21,10 +29,29 @@ public class MainMenuScreen implements Screen{
 
         this.t = t;
         background = new Texture("wallpaper.jpg");
-        muteButton = new Texture("sound.png");
-        startButton = new Texture("startButton.png");
-        highScoresButton = new Texture("hsButton.png");
-        exitButton = new Texture("exitButton.png");
+        muteSprite = new Sprite (new Texture("sound.png"));
+
+
+        startSprite = new Sprite (new Texture("startButton.png"));
+        startSprite.setSize( (float) (Gdx.graphics.getWidth()*0.09375),  (float) (Gdx.graphics.getHeight()*0.0834));
+        startButton = new Button(new SpriteDrawable(startSprite));
+        startButton.setX(Gdx.graphics.getWidth()/2 - startButton.getWidth()/2);
+        startButton.setY(Gdx.graphics.getHeight()/2 - startButton.getHeight()/2 + startButton.getHeight() * (float)1.5);
+
+        highScoresSprite = new Sprite (new Texture("hsButton.png"));
+        highScoresSprite.setSize( (float) (Gdx.graphics.getWidth()*0.09375),  (float) (Gdx.graphics.getHeight()*0.0834));
+        highScoresButton = new Button(new SpriteDrawable(highScoresSprite));
+        highScoresButton.setX(Gdx.graphics.getWidth()/2 - highScoresButton.getWidth()/2);
+        highScoresButton.setY(Gdx.graphics.getHeight()/2 - highScoresButton.getHeight()/2);
+
+        exitSprite = new Sprite(new Texture("exitButton.png"));
+        exitSprite.setSize( (float) (Gdx.graphics.getWidth()*0.09375),  (float) (Gdx.graphics.getHeight()*0.0834));
+        exitButton = new Button(new SpriteDrawable(exitSprite));
+        exitButton.setX(Gdx.graphics.getWidth()/2 - exitButton.getWidth()/2);
+        exitButton.setY(Gdx.graphics.getHeight()/2 - exitButton.getHeight()/2 - exitButton.getHeight() * (float) 1.5);
+
+
+        exitSprite.setSize( (float) (Gdx.graphics.getWidth()*0.09375),  (float) (Gdx.graphics.getHeight()*0.0834));
 
     }
 
@@ -38,10 +65,13 @@ public class MainMenuScreen implements Screen{
 
         t.batch.begin();
         t.batch.draw(background, 0, 0);
-        t.batch.draw(muteButton, 1500, 800);
-        t.batch.draw(startButton, 700, 450, BUTTON_WIDTH, BUTTON_HEIGHT);
-        t.batch.draw(highScoresButton, 700, 350, BUTTON_WIDTH, BUTTON_HEIGHT);
-        t.batch.draw(exitButton, 700, 250, BUTTON_WIDTH, BUTTON_HEIGHT);
+        //background.draw(this.t.batch, );
+        t.batch.draw(muteSprite, 1500, 800);
+        startButton.draw(t.batch, 1);
+        highScoresButton.draw(t.batch, 1);
+        exitButton.draw(t.batch, 1);
+        //t.batch.draw(highScoresSprite, 700, 350, BUTTON_WIDTH, BUTTON_HEIGHT);
+        //t.batch.draw(exitSprite, 700, 250, BUTTON_WIDTH, BUTTON_HEIGHT);
         t.batch.end();
 
     }
--- desktop/src/com/mygdx/mainpackage/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/mainpackage/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new Tetris(), config);
-		config.resizable = false;
-		//TO DO: Tentar por isto adaptável (se tivermos tempo :P )
+		config.resizable = true;
+		//TODO: Tentar por isto adaptável (se tivermos tempo :P )
 		config.width = 1600;
 		config.height = 900;
 		//config.fullscreen = true;
