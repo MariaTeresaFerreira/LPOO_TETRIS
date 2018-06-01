6c07d8d1e399dd44d062bd8b0627d2cc401b9d9d
pppereira3 <=>
2018-06-01 23:31:57 +0100

    Teste android nao da para fazer deste lado

--- core/src/com/mygdx/mainpackage/Screens/MGSAndroid.java
+++ core/src/com/mygdx/mainpackage/Screens/MGSAndroid.java
@@ -1,11 +1,25 @@
 package com.mygdx.mainpackage.Screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.mygdx.mainpackage.Tetris;
 
 public class MGSAndroid extends MainGameScreen {
 
     public MGSAndroid(final Tetris t){
         super(t);
+    }
 
+    @Override
+    public void render(float delta){
+        if (Gdx.input.getGyroscopeX() >= 0.0872665){
+            t.g.shiftLeft();
+        }
+
+        if (Gdx.input.getAccelerometerX() <= -0.0872665){
+            t.g.shiftRight();
+        }
+
+        super.render(delta);
     }
 }
